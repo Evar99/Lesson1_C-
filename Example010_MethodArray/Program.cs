1dd451e05ee53ff85e0f8401f6d2f6866addc07d
Shatilova Vera <egorova_ve@mail.ru>
2022-09-13 18:26:55 +0300

    add Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// ищем, на какой позиции (индекс элемента) стоит число, которое мы ищем
+
+
+int[] array = { 1, 20, 33, 18, 51, 63, 17, 18 };
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если условие уже единожды выполнилось, больше ничего делать не нужно
+    }
+    //index = index + 1;
+    index++;
+}
